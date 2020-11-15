d8a24f349255ee53d618264d798fb713efa1c4bc
chook <dylanw212@gmail.com>
2020-11-15 18:43:01 +1100

    add working files

--- scripts.js
+++ scripts.js
@@ -1,16 +1,19 @@
 let myLibrary = [];
 
+// Constructor
 function Book(title, author, pages, read) {
   this.title = title
   this.author = author
   this.pages = pages
   this.read = read
+  // First way to add functions to an object
   // this.info = function() {
   //  return `${title} by ${author}, ${pages} pages long, ${read ? 'already read' : 'not read yet'}`
   // }
 };
 
-// Alternate / better practice - prototypes
+// Alternate / better practice - prototypes. This way the function is only created once and shared, rather than re-created
+// every instantiation
 Book.prototype.info = function() {
   return `${this.title} by ${this.author}, ${this.pages} pages long, ${this.read ? 'already read' : 'not read yet'}`
 };

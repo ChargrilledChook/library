a529570a9d6c4783d9f721d0067b72d361f78a2c
chook <dylanw212@gmail.com>
2020-12-04 16:58:43 +1100

    form submission prototype done

--- scripts.js
+++ scripts.js
@@ -89,4 +89,23 @@ const displayLibray = function displayLibraryOnDocument(library) {
   }
 };
 
+// Form stuff
+let title;
+
+const submitButton = document.querySelector("#submit-btn");
+
+submitButton.addEventListener("click", (e) => {
+  e.preventDefault();
+  const title = document.querySelector("#title").value;
+  const author = document.querySelector("#author").value;
+  const pages = document.querySelector("#pages").value;
+  const read = document.querySelector("#read").value;
+  const bookToAdd = new Book(title, author, pages, read);
+  myLibrary.push(bookToAdd);
+  libraryContainer.innerHTML = "";
+  displayLibray(myLibrary);
+  saveLibrary();
+});
+// Running the scripts
+
 displayLibray(myLibrary);

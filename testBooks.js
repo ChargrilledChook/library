dc281c892e64023c4938d8cbc37eb50a91b6588b
chook <dylanw212@gmail.com>
2020-12-11 12:32:56 +1100

    remove cruft

---
+++ testBooks.js
@@ -0,0 +1,10 @@
+// For testing
+const hatred = new Book("A little Hatred", "Joe Abercrombie", "350", true);
+const gravity = new Book(
+  "Gravity's Rainbow",
+  "David Foster Wallace",
+  "6780",
+  false
+);
+const prince = new Book("Prince of Thorns", "Mark Lawrence", "280", true);
+const spot = new Book("Spot goes to the Zoo", "Arthur B. Legend", "27", false);
